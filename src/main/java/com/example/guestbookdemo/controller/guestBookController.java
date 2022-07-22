0223d77fdff6174719095ba78b054f955e22ecb9
SierraSon97 <swj1265@naver.com>
2022-07-22 22:39:56 +0900

    master register

--- src/main/java/com/example/guestbookdemo/controller/guestBookController.java
+++ src/main/java/com/example/guestbookdemo/controller/guestBookController.java
@@ -1,5 +1,6 @@
 package com.example.guestbookdemo.controller;
 
+import com.example.guestbookdemo.dto.GuestBookDto;
 import com.example.guestbookdemo.dto.PageRequestDto;
 import com.example.guestbookdemo.service.GuestBookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestBook")
@@ -22,5 +25,19 @@ public class guestBookController {
         log.info("mainMenu-----------");
         model.addAttribute("result", guestBookService.getList(pageRequestDto));
         return "guestBook/main";
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestBookDto guestBookDto, RedirectAttributes redirectAttributes){
+        log.info("dto..."+guestBookDto);
+
+        Long gno = guestBookService.register(guestBookDto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestBook/main";
     }
 }
